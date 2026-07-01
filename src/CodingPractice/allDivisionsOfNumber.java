45b5bd3d31a8a3da1632b855c53c14972f1609b2
PSagarJ <sagarjp369@gmail.com>
2026-07-01 15:08:53 +0530

    done divisors of number problem from coding ninjas

--- src/CodingPractice/allDivisionsOfNumber.java
+++ src/CodingPractice/allDivisionsOfNumber.java
@@ -1,13 +1,31 @@
 package CodingPractice;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.*;
+
 public class allDivisionsOfNumber {
     public void checkDivisors(){
         int n = 36;
+        List<Integer> largeDivisiors = new ArrayList<>();
 
-        for(int i = 1; i <=n; i++){
-            if(n%i == 0){
+//        for(int i = 1; i <=n; i++){
+//            if(n%i == 0){
+//                System.out.println(i);
+//            }
+//        }
+        for (int i =1; i <= sqrt(n); i++){
+            if(n % i == 0){
                 System.out.println(i);
+                if( n / i != i){
+                    //System.out.println(n/i);
+                    largeDivisiors.add(n / i);
+                }
             }
+        }
+        for (int i = largeDivisiors.size() -1; i>=0; i--){
+            System.out.println(largeDivisiors.get(i));
         }
     }
 
