04c7634529e588976c92cfe71e0148a1d1bb346b
PSagarJ <sagarjp369@gmail.com>
2026-07-02 14:38:44 +0530

    done code on LexiGraphcalOrder

--- src/DSA_practice/Day_9.java
+++ src/DSA_practice/Day_9.java
@@ -1,22 +1,40 @@
 package DSA_practice;
 
 public class Day_9 {
+//    public static void main(String[] args) {
+//        int n = 3;
+//        int cr = 0, cc = 0;
+//
+//        find("",cr,cc,n);
+//    }
+//    public static void find(String ans, int cr, int cc, int n){
+//        if(cr == n-1 && cc == n-1){
+//            System.out.print(" "+ans);
+//            return;
+//        }
+//        if(cr >= n || cc>=n)return;
+//
+//        //horizontal
+//        find(ans + "H",cr,cc+1,n);
+//        //vertical
+//        find(ans+ "V",cr+1,cc,n);
+//    }
+
     public static void main(String[] args) {
-        int n = 3;
-        int cr = 0, cc = 0;
+        //LexiGraphcalOrder
 
-        find("",cr,cc,n);
-    }
-    public static void find(String ans, int cr, int cc, int n){
-        if(cr == n-1 && cc == n-1){
-            System.out.print(" "+ans);
-            return;
+        for(int i = 1; i<=9; i++){
+            create(i, 113);
         }
-        if(cr >= n || cc>=n)return;
+    }
+    public static void create(int num, int n){
+        if(num>n) return;
 
-        //horizontal
-        find(ans + "H",cr,cc+1,n);
-        //vertical
-        find(ans+ "V",cr+1,cc,n);
+        System.out.println(num);
+        num=num*10;
+
+        for(int i = 0;i<=9;i++){
+            create(num+i,n);
+        }
     }
 }
