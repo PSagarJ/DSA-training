66d6766ec062a00c1b32377b27e17d4c8314bde1
PSagarJ <sagarjp369@gmail.com>
2026-07-03 10:32:31 +0530

    Day_10, backtracking practice problem 1. Qeen on chair

--- src/DSA_practice/Day_10.java
+++ src/DSA_practice/Day_10.java
@@ -1,6 +1,24 @@
 package DSA_practice;
 
 public class Day_10 {
+    public static void main(String[] args) {
+        boolean chair [] = new boolean[3];
+        seat("",1,2,chair);
+    }
+    public static void seat(String ans, int q, int n, boolean chair[]){
 
+        if(q > n){
+            System.out.println(ans);
+            return;
+        }
+        for(int i = 0; i<chair.length; i++){
+            if(chair[i] == false){
+                chair[i] = true;
+
+                seat(ans+("C"+i+"Q"+q),q+1,n,chair);
+                chair[i] = false;
+            }
+        }
+    }
 
 }
