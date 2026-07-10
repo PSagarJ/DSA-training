fe69798143261c6ab1b00a489bdee2822ed9187c
PSagarJ <sagarjp369@gmail.com>
2026-07-10 21:06:17 +0530

    Update maximunsubarray problem started

--- src/Learning_with_claude/MaximumSumSubarray.java
+++ src/Learning_with_claude/MaximumSumSubarray.java
@@ -1,7 +1,37 @@
 package Learning_with_claude;
 
 public class MaximumSumSubarray {
-    public void subarray(int [] arr){
-        //
+
+    public int subarray(int [] arr, int k){
+
+        int windowSum = 0;
+
+        for(int i =0; i<k; i++){
+            windowSum = windowSum + arr[i];
+        }
+
+        int maxSum = windowSum;
+
+        
+
+//        for(int i = 0; i<=arr.length-k; i++){
+//            int newSum = 0;
+//
+//            for(int j = i; j < i + k; j++){
+//                newSum = newSum+arr[j];
+//            }
+//
+//            if(newSum > maxSum) {
+//                maxSum = newSum;
+//            }
+//        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {2, 1, 5, 1, 3, 2};
+        MaximumSumSubarray ms = new MaximumSumSubarray();
+
+        System.out.println("Max sum of subarray: "+ms.subarray(arr,3));
     }
 }
