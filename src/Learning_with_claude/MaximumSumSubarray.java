c3176b5c46a7b3d66c8764c7a11f03c935ac137f
PSagarJ <sagarjp369@gmail.com>
2026-07-11 10:57:58 +0530

    Done code on MaxSumSubarray

--- src/Learning_with_claude/MaximumSumSubarray.java
+++ src/Learning_with_claude/MaximumSumSubarray.java
@@ -6,13 +6,20 @@ public class MaximumSumSubarray {
 
         int windowSum = 0;
 
-        for(int i =0; i<k; i++){
-            windowSum = windowSum + arr[i];
+        for(int i = 0; i<k; i++){  // 0 1 2 < 3
+            windowSum = windowSum + arr[i]; // 0+2 = 2 -> 2+1 = 3 -> 3+5=8 end
         }
 
         int maxSum = windowSum;
 
-        
+        for(int i = k; i<arr.length; i++){
+            windowSum = windowSum -arr[i-k]+arr[i];
+            if(windowSum>maxSum) maxSum = windowSum;
+        }
+        return maxSum;
+
+
+
 
 //        for(int i = 0; i<=arr.length-k; i++){
 //            int newSum = 0;
@@ -25,11 +32,10 @@ public class MaximumSumSubarray {
 //                maxSum = newSum;
 //            }
 //        }
-        return maxSum;
     }
 
     public static void main(String[] args) {
-        int [] arr = {2, 1, 5, 1, 3, 2};
+        int [] arr = {1,2,4,1,2,4,2,4,9,9,3,5,3,2,3,4,5,3,5,9};
         MaximumSumSubarray ms = new MaximumSumSubarray();
 
         System.out.println("Max sum of subarray: "+ms.subarray(arr,3));
